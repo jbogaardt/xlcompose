5a58ec60a75fbaf54d393d967538ac442ee9e35e
John S Bogaardt <jbogaardt@gmail.com>
2019-12-09 19:36:59 -0700

    Update __init__.py

--- xlcompose/__init__.py
+++ xlcompose/__init__.py
@@ -1,3 +1,4 @@
 __version__ = '0.1.2'
 from xlcompose.core import (
-    DataFrame, Series, Row, Column, Tabs, CSpacer, RSpacer, Title, Image, Sheet)
+    DataFrame, Series, Row, Column, Tabs, CSpacer, RSpacer, Title, Image,
+    Sheet, VSpacer, HSpacer)
